792a151e5f945d6fe8d8171812fa86f80fef100a
Baiketli Dijo <baiketlid@gmail.com>
2019-03-10 14:12:34 +0000

    changed StackPane to FlowPane

--- src/stages/BlankStage.java
+++ src/stages/BlankStage.java
@@ -3,9 +3,12 @@ package stages;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,6 +17,7 @@ import javafx.stage.Stage;
  */
 public class BlankStage extends Application {
     private Label myLabel;
+    private Label myLabel2;
 
     @Override
     public void start(Stage stage) {
@@ -25,17 +29,25 @@ public class BlankStage extends Application {
         myLabel = new Label();
         myLabel.setText("This is a label");
 
+//        myLabel2 = new Label();
+//        myLabel2.setText("Label 2");
+
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+                myLabel.setText("This is a label");
                 System.out.println("Hello JavaFX");
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-        root.getChildren().add(myLabel);
-        Scene scene = new Scene(root, 250, 200);
+        FlowPane flowPaneRoot = new FlowPane(10, 10);
+        flowPaneRoot.setAlignment(Pos.CENTER);
+
+//        StackPane root = new StackPane();
+        flowPaneRoot.getChildren().add(button);
+        flowPaneRoot.getChildren().add(myLabel);
+//        flowPaneRoot.getChildren().add(myLabel2);
+        Scene scene = new Scene(flowPaneRoot, 250, 200);
         stage.setScene(scene);
         stage.show();
     }
