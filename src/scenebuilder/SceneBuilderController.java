fa38e38c7d841395a678b6c9fc623d9936cb5324
Baiketli Dijo <baiketlid@gmail.com>
2019-03-11 15:44:13 +0000

    - added demo of using scenebuilder.

---
+++ src/scenebuilder/SceneBuilderController.java
@@ -0,0 +1,4 @@
+package scenebuilder;
+
+public class SceneBuilderController {
+}
---
+++ src/scenebuilder/SceneBuilderDemo.java
@@ -0,0 +1,18 @@
+package scenebuilder;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author baike
+ */
+public class SceneBuilderDemo extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
