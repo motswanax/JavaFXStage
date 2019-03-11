e50822e62a3613c7318776dbc758fb32f85b3363
Baiketli Dijo <baiketlid@gmail.com>
2019-03-11 16:10:17 +0000

    - linked scenebuilder.fxml to the Controller - implemented Initializable in the Controller and added an EventHandler to the clickMeButton, which modifies the label - added fx:id to the components using SceneBuilder - modified the dimensions of the app to 700x400

--- src/scenebuilder/SceneBuilderController.java
+++ src/scenebuilder/SceneBuilderController.java
@@ -1,4 +1,30 @@
 package scenebuilder;
 
-public class SceneBuilderController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SceneBuilderController implements Initializable {
+
+    @FXML
+    private Button clickMeButton;
+
+    @FXML
+    private Label labelId;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                labelId.setText("Hey there");
+            }
+        });
+    }
 }
--- src/scenebuilder/SceneBuilderDemo.java
+++ src/scenebuilder/SceneBuilderDemo.java
@@ -1,6 +1,9 @@
 package scenebuilder;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -9,7 +12,10 @@ import javafx.stage.Stage;
 public class SceneBuilderDemo extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("scenebuilder.fxml"));
+        stage.setTitle("Hello Scene Builder");
+        stage.setScene(new Scene(root, 700, 400));
+        stage.show();
     }
 
     public static void main(String[] args) {
