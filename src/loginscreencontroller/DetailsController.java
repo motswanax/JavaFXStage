42d900023fb3d44ee8047a7f5ac95d5c59275b47
Baiketli Dijo <baiketlid@gmail.com>
2019-03-13 17:08:40 +0000

    - added loginUser() to LoginScreenController, which switches to the details view on click of the login button. - implemented the intitizlize() in the DetailsController - added event handlers to the url and twitter labels that open hyperlinks in the browser.

--- src/loginscreencontroller/DetailsController.java
+++ src/loginscreencontroller/DetailsController.java
@@ -1,7 +1,91 @@
 package loginscreencontroller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * @author baike
  */
 public class DetailsController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label detailsWelcomeLabel;
+
+    @FXML
+    private Label detailsName;
+
+    @FXML
+    private Label detailsAge;
+
+    @FXML
+    private Label detailsProfession;
+
+    @FXML
+    private Hyperlink detailsWeb;
+
+    @FXML
+    private Hyperlink detailsTwitter;
+
+    @FXML
+    void initialize() {
+
+        detailsTwitter.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (Desktop.isDesktopSupported()) {
+                    try {
+                        Desktop.getDesktop().browse(new URI("https://www.instagram.com/simpleviablesolutions/"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        detailsWeb.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println(detailsWeb.getText());
+                if (Desktop.isDesktopSupported()) {
+                    try {
+                        Desktop.getDesktop().browse(new URI("www.simplesolutionsbw.com"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void setDetailsName(String name, String profession, int age) {
+        detailsWelcomeLabel.setText("Welcome " + formatName(name));
+        detailsName.setText(formatName(name));
+        detailsAge.setText(Integer.toString(age));
+        detailsProfession.setText(profession);
+    }
+
+    private String formatName(String name) {
+        if (name.length() == 0) return name;
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
 }
--- src/loginscreencontroller/LoginScreenController.java
+++ src/loginscreencontroller/LoginScreenController.java
@@ -6,7 +6,12 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -38,9 +43,38 @@ public class LoginScreenController {
         loginButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println("Login was clicked");
+                loginUser();
             }
         });
 
+    }
+
+    private void loginUser() {
+
+        // Get the main window
+        loginButton.getScene().getWindow().hide();
+
+        if (!loginUsername.getText().trim().equals("") && !loginPassword.getText().trim().equals("")) {
+            // create new stage
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/loginscreenview/details.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setTitle("Details");
+            stage.setScene(new Scene(root));
+
+            // Get the controller
+            DetailsController detailsController = loader.getController();
+            detailsController.setDetailsName(loginUsername.getText(), "Java Developer", 31);
+
+            stage.show();
+        }
     }
 }
