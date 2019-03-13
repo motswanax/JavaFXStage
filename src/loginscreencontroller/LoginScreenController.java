ad5b14bfdf1920677b9583013b56d4559b848a53
Baiketli Dijo <baiketlid@gmail.com>
2019-03-13 13:42:36 +0000

    - added assets package and added image icons for the UI - added UI components to the Login screen - initialized UI components in the LoginScreenController and added an event handler to the loginButton

--- src/loginscreencontroller/LoginScreenController.java
+++ src/loginscreencontroller/LoginScreenController.java
@@ -1,7 +1,46 @@
 package loginscreencontroller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * @author baike
  */
 public class LoginScreenController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField loginUsername;
+
+    @FXML
+    private JFXPasswordField loginPassword;
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    void initialize() {
+
+        loginUsername.setStyle("-fx-text-fill: #a0a2ab");
+        loginPassword.setStyle("-fx-text-fill: #a0a2ab");
+
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Login was clicked");
+            }
+        });
+
+    }
 }
