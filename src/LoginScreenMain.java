2daab7bdf81b8e8b4ff95b8be97292c5d3883737
Baiketli Dijo <baiketlid@gmail.com>
2019-03-13 12:07:27 +0000

    - created new "Project" in the loginscreenview package - linked LoginScreenController to loginscreen.fxml - added the main class LoginScreenMain - added login UI components in Scenebuilder

---
+++ src/LoginScreenMain.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author baike
+ */
+public class LoginScreenMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/loginscreenview/loginscreen.fxml"));
+        stage.setTitle("Yo What Up");
+        stage.setScene(new Scene(root, 700, 400));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
